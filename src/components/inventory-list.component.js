dbe082d3eaa76ad78720c6b9a6ed4102cdb0c965
Yifeng Hou <yifeng@Yifengs-iMac.local>
2019-10-02 12:47:22 +0800

    Show inventory amount

--- src/components/inventory-list.component.js
+++ src/components/inventory-list.component.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import { getAllInventory } from '../services/inventory-service'
 
 export default class InventoryList extends Component {
@@ -10,14 +12,28 @@ export default class InventoryList extends Component {
     componentDidMount() {
         getAllInventory()
             .then(res => {
-                const inventories = res.data.map((i) => ({ id: i._id, description: i.description }))
+                const inventories = res.data.map((i) => ({
+                    id: i._id,
+                    description: i.description,
+                    amount: i.amount
+                }))
                 this.setState({ inventories })
             })
     }
 
-    showInventoryList() {
+    showInventoryTypes() {
         const listItems = this.state.inventories
-            .map(inventory => <ListGroup.Item key={inventory.id}>{inventory.description}</ListGroup.Item>)
+            .map(inventory => <ListGroup.Item key={inventory.id + '_type'}>{inventory.description}</ListGroup.Item>)
+        return (
+            <ListGroup>
+                {listItems}
+            </ListGroup>
+        )
+    }
+
+    showInventoryAmounts() {
+        const listItems = this.state.inventories
+            .map(inventory => <ListGroup.Item key={inventory.id + '_amount'} variant='info'>{inventory.amount}</ListGroup.Item>)
         return (
             <ListGroup>
                 {listItems}
@@ -29,7 +45,10 @@ export default class InventoryList extends Component {
         return (
             <div>
                 <p>Welcome to Inventory List Component!!</p>
-                {this.showInventoryList()}
+                <Row>
+                    <Col>{this.showInventoryTypes()}</Col>
+                    <Col>{this.showInventoryAmounts()}</Col>
+                </Row>
             </div>
         )
     }
