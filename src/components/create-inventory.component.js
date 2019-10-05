e56e9a0cc8ca993bec3371883617c05cdfd02364
Yifeng Hou <yifeng@Yifengs-iMac.local>
2019-10-05 15:58:42 +0800

    Render inventory items in groups

--- src/components/create-inventory.component.js
+++ src/components/create-inventory.component.js
@@ -1,50 +1,60 @@
 import React, { Component } from 'react';
 import { createInventories } from '../services/inventory-service'
+import { Accordion, Card, } from 'react-bootstrap'
+import './create-inventory.component.css'
 
-const listOfInventoriesTypes = [
-    'Chipsmore',
-    'Oreo box',
-    'Monde Pola 10',
+const groupsOfInventoryTypes = {
+    'Cabinet 1': [
+        'Chipsmore',
+        'Oreo box',
+        'Monde Pola 10',
 
-    'Santino Expresso Bar',
-    'Boncafe Coffee Beans',
-    'Milk For Coffee Machine 12',
-
-    'Orange',
-    'Lemon',
-    'Grape',
-    'Coca Cola',
-
-    'Milo',
-    'Soya Bean',
-    'Teh Tarik',
-
-    'Milk & Cream 12',
-    'Chocolate Banana 12',
-    'MERLION Durian 15',
-    'Angels Berry 12',
-    'Chocolate 15',
-    'Mango & Passion Fruit 15',
-    'Strawberry Cheesecake 15',
-    'Vanilla 15',
-
-    'Shrimp Wonton Noodles',
-    'Spaghetti with Chicken Sauce',
-    'Stir Fried Chicken & Basil with Rice',
-
-    'Water 24',
-    'Coke',
-    'Tea',
-
-    'Peanut Butter',
-    'Fish biscuits',
-    'Lemon puffs',
-    'Egg Crackers',
-    'Love Letters',
-    'Iced Jems',
-    'Butterfly',
-    'Potato Sticks'
-]
+        'Santino Expresso Bar',
+        'Boncafe Coffee Beans',
+        'Milk For Coffee Machine 12',
+    ],
+    'Cabinet 2': [
+        'Orange',
+        'Lemon',
+        'Grape',
+        'Coca Cola',
+    ],
+    'Cabinet 3': [
+        'Milo',
+        'Soya Bean',
+        'Teh Tarik',
+    ],
+    'Small Fridge': [
+        'Shrimp Wonton Noodles',
+        'Spaghetti with Chicken Sauce',
+        'Stir Fried Chicken & Basil with Rice',
+    ],
+    'Large Fridge': [
+        'Milk & Cream 12',
+        'Chocolate Banana 12',
+        'MERLION Durian 15',
+        'Angels Berry 12',
+        'Chocolate 15',
+        'Mango & Passion Fruit 15',
+        'Strawberry Cheesecake 15',
+        'Vanilla 15',
+    ],
+    'Drinks': [
+        'Water 24',
+        'Coke',
+        'Tea',
+    ],
+    'Cookies': [
+        'Peanut Butter',
+        'Fish biscuits',
+        'Lemon puffs',
+        'Egg Crackers',
+        'Love Letters',
+        'Iced Jems',
+        'Butterfly',
+        'Potato Sticks'
+    ]
+}
 
 export default class CreateInventory extends Component {
     constructor(props) {
@@ -83,8 +93,8 @@ export default class CreateInventory extends Component {
         })
     }
 
-    renderInventoryInputs() {
-        return listOfInventoriesTypes.map(inventoryType => (
+    _renderInventoryItems(inventoryTypes) {
+        return inventoryTypes.map(inventoryType => (
             <div className="form-group" key={inventoryType}>
                 <label>{inventoryType}: </label>
                 <input type="number"
@@ -96,13 +106,37 @@ export default class CreateInventory extends Component {
         ))
     }
 
+    _renderInventoryGroup(groupType, inventoryTypes) {
+        return (
+            <Card key={groupType}>
+                <Accordion.Toggle as={Card.Header} eventKey={groupType}>
+                    {groupType}
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey={groupType}>
+                    <Card.Body>{this._renderInventoryItems(inventoryTypes)}</Card.Body>
+                </Accordion.Collapse>
+            </Card>
+        )
+    }
+
+    renderInventoryGroups() {
+        return (
+            <Accordion>
+                {Object.entries(groupsOfInventoryTypes)
+                    .map(([groupType, inventoryTypes]) => this._renderInventoryGroup(groupType, inventoryTypes))}
+            </Accordion>
+        )
+    }
+
     render() {
         return (
             <div style={{ marginTop: 10 }}>
                 <h3>Create New Inventory</h3>
                 <form onSubmit={this.onSubmit}>
-                    {this.renderInventoryInputs()}
-                    <div className="form-group">
+                    <div className="form-fields-container">
+                        {this.renderInventoryGroups()}
+                    </div>
+                    <div className="form-group submit-button-container">
                         <input type="submit" value="Create Inventory" className="btn btn-primary" />
                     </div>
                 </form>
