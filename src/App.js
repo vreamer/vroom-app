be08dcefb40f3b35e2c2754ac9a7d5b15b1641df
Grace <hyf94244280@gmail.com>
2019-10-14 15:51:02 +0800

    Change website to v room logo and favicon

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank" rel="noopener noreferrer">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            <a className="navbar-brand" href="https://vroom-app.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+              <img src={logo} width="30" height="30" />
             </a>
             <Link to="/" className="navbar-brand">V Room App</Link>
             <div className="collpase navbar-collapse">
