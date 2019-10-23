78bb7b78c92631fae681aad781cf57c4d56ac426
Grace <hyf94244280@gmail.com>
2019-10-23 16:11:33 +0800

    Increment inventory amount on inventory list
    [#169311233]

--- src/components/inventory-list.component.js
+++ src/components/inventory-list.component.js
@@ -1,42 +1,86 @@
 import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table'
-import { getInventoryByDate } from '../services/inventory-service'
+import { getInventoryByDate, createInventories } from '../services/inventory-service'
 import { getTodayISO } from '../services/date-service'
 import InventoryCopy from './inventory-copy.component'
 import './inventory-list.component.css'
+import { toast } from 'react-toastify';
 
 const today = getTodayISO()
 
 export default class InventoryList extends Component {
     state = {
-        inventories: []
+        inventories: [],
+        newInventories: {}
     }
 
     componentDidMount() {
+        this._loadInventories()
+    }
+
+    _loadInventories() {
         getInventoryByDate(today)
-            .then(res => {
-                const inventories = res.data.map((i) => ({
-                    id: i.description,
-                    description: i.description,
-                    amount: i.amount,
-                    hasStockIn: i.hasStockIn
-                }))
-                this.setState({ inventories })
-            })
+        .then(res => {
+            const inventories = res.data.map((i) => ({
+                id: i.description,
+                description: i.description,
+                amount: i.amount,
+                hasStockIn: i.hasStockIn
+            }))
+            this.setState({ inventories })
+        })
     }
 
     _isInventoryInDanger(amount) {
         return amount > 0 && amount <= 0.5
     }
+    
+    _renderAddInventory(inventory) {
+        console.log(inventory)
+        return (
+            <div>
+                <input type="number"
+                    value={this.state.newInventories[inventory.description] || ''}
+                    onChange={(e) => this.onChangeInventory(e, inventory)}
+                />
+            </div>
+        )
+    }
 
     _renderInventories() {
         return this.state.inventories
             .map(inventory => (
-                <tr class={this._isInventoryInDanger(inventory.amount) ? 'inventory-in-danger' : ''} key={inventory.id}>
+                <tr className={this._isInventoryInDanger(inventory.amount) ? 'inventory-in-danger' : ''} key={inventory.id}>
                     <td>{inventory.description}</td>
                     <td>{inventory.amount}</td>
+                    <td>{this._renderAddInventory(inventory)}</td>
                 </tr>
             ))
+    }
+
+    onChangeInventory(e, inventory) {
+        const prevInventory = this.state.newInventories
+        this.setState({
+            newInventories: {
+                ...prevInventory,
+                [inventory.description]: e.target.value
+            }
+        });
+    }
+
+    saveInventories() {
+        const inventories = Object.entries(this.state.newInventories)
+            .map(([inventoryTitle, amount]) => ({
+                description: inventoryTitle,
+                amount: amount,
+                date: today
+            }))
+        createInventories(inventories)
+            .then(_ => {
+                toast.success("Inventory updated successfully");
+                this.setState({ newInventories: {} })
+                this._loadInventories()
+            })
     }
 
     render() {
@@ -49,6 +93,7 @@ export default class InventoryList extends Component {
                         <tr>
                             <th>Name</th>
                             <th>Amount</th>
+                            <th><button className='btn btn-primary' onClick={this.saveInventories.bind(this)}>Add</button></th>
                         </tr>
                     </thead>
                     <tbody>
