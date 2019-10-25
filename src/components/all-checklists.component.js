87c7fc225a4a8c4349e4a103c8c433cab4f72b10
Grace <hyf94244280@gmail.com>
2019-10-25 12:20:12 +0800

    Show checklist step details in modal
    [#169357793]

--- src/components/all-checklists.component.js
+++ src/components/all-checklists.component.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { getChecklists } from '../services/checklist-service'
-import { Accordion, Card, Button, ListGroup } from 'react-bootstrap'
+import { Accordion, Card, Button, ListGroup, Modal } from 'react-bootstrap'
+import './all-checklists.component.css'
 
 export default class AllChecklists extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            checklists: []
+            checklists: [],
+            showStepDetails: false,
+            currentStepDetails: {}
         }
+        this.open = this.open.bind(this)
+        this.close = this.close.bind(this)
     }
 
     componentDidMount() {
@@ -17,13 +22,18 @@ export default class AllChecklists extends React.Component {
 
     _renderChecklists() {
         const checklists = this.state.checklists.map(c => (
-            <Card>
+            <Card key={c.title}>
                 <Accordion.Toggle as={Card.Header} eventKey={c.title}>
                     {c.title}</Accordion.Toggle>
                 <Accordion.Collapse eventKey={c.title}>
                     <Card.Body>
                         <ListGroup>
-                            {c.steps.map(s => <ListGroup.Item>{s}</ListGroup.Item>)}
+                            {c.steps.map(s => (
+                                <ListGroup.Item key={s.title} className="checklist-step-list-item">
+                                    {s.title}
+                                    <Button onClick={() => this.open(s)}>Show Details</Button>
+                                </ListGroup.Item>
+                            ))}
                         </ListGroup>
                     </Card.Body>
                 </Accordion.Collapse>
@@ -36,10 +46,33 @@ export default class AllChecklists extends React.Component {
         )
     }
 
+    _renderStepDetailModal() {
+        return (
+            <Modal show={this.state.showStepDetails} onHide={this.close}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{this.state.currentStepDetails.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <img className='step-details-image' src={this.state.currentStepDetails.image} />
+                    {this.state.currentStepDetails.description}
+                </Modal.Body>
+            </Modal>
+        )
+    }
+
+    open(step) {
+        this.setState({ showStepDetails: true, currentStepDetails: step })
+    }
+
+    close() {
+        this.setState({ showStepDetails: false, currentStepDetails: {} })
+    }
+
     render() {
         return (
             <div>
                 {this._renderChecklists()}
+                {this._renderStepDetailModal()}
             </div>
         )
     }
