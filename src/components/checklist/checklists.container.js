c36b4612459be5260702259267d9a9d132c2bb54
Vreamer <yhou.vreamer@gmail.com>
2019-10-27 22:25:49 +0800

    Update step title
    [#169243040]

--- src/components/checklist/checklists.container.js
+++ src/components/checklist/checklists.container.js
@@ -13,9 +13,14 @@ export default class ChecklistContainer extends React.Component {
         }
 
         this.toggleUpdateChecklists = this.toggleUpdateChecklists.bind(this)
+        this.loadChecklists = this.loadChecklists.bind(this)
     }
 
     componentDidMount() {
+        this.loadChecklists()
+    }
+
+    loadChecklists() {
         getChecklists()
             .then(res => this.setState({ checklists: res.data }))
     }
@@ -31,7 +36,7 @@ export default class ChecklistContainer extends React.Component {
                     {this.state.updateChecklists ? 'Finish Updates' : 'Update Checklists'}</Button>
 
                 {this.state.updateChecklists ?
-                    <UpdateChecklists checklists={this.state.checklists} />
+                    <UpdateChecklists checklists={this.state.checklists} reloadChecklists={this.loadChecklists} />
                     :
                     <AllChecklists checklists={this.state.checklists} />
                 }
--- src/services/checklist-service.js
+++ src/services/checklist-service.js
@@ -5,3 +5,7 @@ const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:4000'
 export const getChecklists = () => {
     return axios.get(`${apiUrl}/checklist`)
 }
+
+export const updateChecklistStep = (stepTitle, checklistId, stepId) => {
+    return axios.put(`${apiUrl}/checklist/${checklistId}/steps/${stepId}`, { title: stepTitle })
+}
